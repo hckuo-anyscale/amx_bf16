e440459d7adb718ff8d4275a566d3ea34afaf55c
Austin Kuo <hckuo@anyscale.com>
2023-12-09 20:34:48 -0800

    fixes

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAG = -march=sapphirerapids -fno-strict-aliasing -mamx-tile -mamx-int8 -mamx-bf16 -std=c++23 -mavx512fp16
+CFLAG = -march=sapphirerapids -fno-strict-aliasing -mamx-tile -mamx-int8 -mamx-bf16 -std=c++23 -mavx512fp16 -O3
 CC = g++
 BIN = test-amxtile 
 CFILES =test-amxtile.cpp
--- test-amxtile.cpp
+++ test-amxtile.cpp
@@ -22,7 +22,7 @@
 #define XFEATURE_XTILEDATA      18
 using namespace std;
 
-template <typename S>
+	template <typename S>
 ostream& operator<<(ostream& os,
 		const vector<S>& vector)
 {
@@ -93,10 +93,36 @@ static void init_tile_config (__tilecfg *tileinfo)
 	_tile_loadconfig(tileinfo);
 }
 
+void tile_dp(float16_t *m1, float16_t *m2, float32_t *res) {
+	// Load tile configuration 
+	// Load tile data
+	_tile_loadd(1, res, STRIDE);
+	_tile_loadd(2, m1, STRIDE);
+	_tile_loadd(3, m2, STRIDE);
+	// Compute dot-product of bytes in tiles 
+	_tile_dpbf16ps (1, 2, 3);
+	// Store the tile data to memory
+	_tile_stored (1, res, STRIDE);
+	return ;
+}
+
+void slow_dp(float16_t *m1, float16_t *m2, float32_t *res) {
+	// Perform the dot product
+    int rowsA = 16;
+    int colsA = 32;
+    int colsB = 16;
+    for (size_t i = 0; i < rowsA; ++i) {
+        for (size_t j = 0; j < colsB; ++j) {
+            for (size_t k = 0; k < colsA; ++k) {
+                res[i*(rowsA)+j] += (m1[i*colsA+k] * m2[k*colsB+j]);
+            }
+        }
+    }
+}
 int main(){
 	__tilecfg tile_data = {0};
-	vector<bfloat16_t> src1(16 * 32, 1.0bf16);
-	vector<bfloat16_t> src2(16 * 32, 1.0bf16);
+	vector<float16_t> src1(16 * 32, 1.0f16);
+	vector<float16_t> src2(16 * 32, 1.0f16);
 	vector<float32_t> res(16 * 16, 0.0f32);
 
 	cout << src1 << endl;
@@ -105,21 +131,16 @@ int main(){
 
 
 	// Request permission to linux kernel to run AMX 
-	if (!set_tiledata_use())
+	if (!set_tiledata_use()) {
 		exit(-1);
+	}
+
 	auto start = std::chrono::high_resolution_clock::now();
-	auto iterations = 100000;
+	auto iterations = 10000;
 	init_tile_config (&tile_data);
 	for (int i = 0; i < iterations; i++) {
-		// Load tile configuration 
-		// Load tile data
-		_tile_loadd(1, res.data(), STRIDE);
-		_tile_loadd(2, src1.data(), STRIDE);
-		_tile_loadd(3, src2.data(), STRIDE);
-		// Compute dot-product of bytes in tiles 
-		_tile_dpbf16ps (1, 2, 3);
-		// Store the tile data to memory
-		_tile_stored (1, res.data(), STRIDE);
+		//tile_dp(src1.data(), src2.data(), res.data());
+		slow_dp(src1.data(), src2.data(), res.data());
 	}
 	auto end = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
